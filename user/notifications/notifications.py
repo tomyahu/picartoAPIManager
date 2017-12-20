80ce7594091bfd4fea43f4a610dffde67e001047
tomyahu <tomyahu@gmail.com>
2017-12-20 16:43:34 -0300

    renamed notification input to data

---
+++ user/notifications/notifications.py
@@ -0,0 +1,25 @@
+import jsonManager as jsonM
+
+class Notification():
+
+    def __init__(self, data):
+        self.channel = data['channel']
+        self.type = data['type']
+        self.read = not data['unread']
+        self.url = data['uri']
+        self.icon = data['has_icon']
+
+    def get_channel(self):
+        return self.channel
+
+    def get_type(self):
+        return self.type
+
+    def get_url(self):
+        return self.url
+
+    def is_read(self):
+        return self.read
+
+    def has_icon(self):
+        return self.icon
