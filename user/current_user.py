c114e8d976f886c9a8b00edf1d218e31a46f6309
tomyahu <tomyahu@gmail.com>
2017-12-20 16:36:00 -0300

    Added diferent users

---
+++ user/current_user.py
@@ -0,0 +1,64 @@
+import jsonManager as jsonM
+from channel.channel import Channel
+from user import RegularUser
+
+
+class CurrentUser():
+
+    def __init__(self):
+        data = jsonM.get_from_url('https://api.picarto.tv/v1/user')
+        self.channel = Channel(data['channel_details'])
+        self.email = data['email']
+        self.creation_date = data['creation_date']
+        self.private_key = data['private_key']
+        self.nsfw_enabled = data['nsfw_enabled']
+        self.nsfw_app = data['nsfw_app']
+        self.follows = []
+
+        for follow in data['following']:
+            self.follows += [RegularUser(follow)]
+
+        self.followers = []
+
+        for follower in data['followers']:
+            self.followers += [RegularUser(follower)]
+
+        self.subscriptions = []
+
+        for subscription in data['subscriptions']:
+            self.subscriptions += [RegularUser(subscription)]
+
+        self.subscribers = []
+
+        for subscriber in data['subscribers']:
+            self.subscribers += [RegularUser(subscriber)]
+
+    def get_email(self):
+        return self.email
+
+    def get_channel(self):
+        return self.channel
+
+    def get_creation_date(self):
+        return self.creation_date
+
+    def get_private_key(self):
+        return self.private_key
+
+    def is_nsfw_enabled(self):
+        return self.nsfw_enabled
+
+    def is_using_nsfw_app(self):
+        return self.nsfw_app
+
+    def get_follows(self):
+        return self.follows
+
+    def get_followers(self):
+        return self.followers
+
+    def get_subscriptions(self):
+        return self.subscriptions
+
+    def get_subscribers(self):
+        return self.subscribers
--- user/user.py
+++ user/user.py
@@ -1,5 +1,38 @@
+import abc
 
 
-class user():
-    
-    def __init__
+class AbstractUser(abc.ABCMeta):
+
+    def __init__(self, data):
+        self.id = data['user_id']
+        self.name = data['name']
+        self.online = data['online']
+
+    def get_id(self):
+        return self.id
+
+    def get_name(self):
+        return self.name
+
+    def is_online(self):
+        return self.online
+
+
+class RegularUser(AbstractUser):
+
+    def __init__(self, data):
+        AbstractUser.__init__(data)
+        self.avatar_url = data['avatar']
+
+    def get_avatar_url(self):
+        return self.avatar_url
+
+
+class MultistreamUser(AbstractUser):
+
+    def __init__(self, user):
+        AbstractUser.__init__(data)
+        self.adult = user['adult']
+
+    def is_adult(self):
+        return self.adult
